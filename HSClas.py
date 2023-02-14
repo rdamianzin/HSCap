9931fb7d2b0b451b150262954e58ebcd3a364725
Roberto Damian <122555791+rdamianzin@users.noreply.github.com>
2023-02-14 13:16:50 -0600

    Add files via upload

---
+++ HSClas.py
@@ -0,0 +1,31 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QMainWindow, QStatusBar, QToolBar
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__(parent=None)
+        self.setWindowTitle("IFUNAM")
+        self.setCentralWidget(QLabel("I'm the Central Widget"))
+        self._createMenu()
+        self._createToolBar()
+        self._createStatusBar()
+
+    def _createMenu(self):
+        menu = self.menuBar().addMenu("&Menu")
+        menu.addAction("&Exit", self.close)
+
+    def _createToolBar(self):
+        tools = QToolBar()
+        tools.addAction("Exit", self.close)
+        self.addToolBar(tools)
+
+    def _createStatusBar(self):
+        status = QStatusBar()
+        status.showMessage("I'm the Status Bar")
+        self.setStatusBar(status)
+
+if __name__ == "__main__":
+    app = QApplication([])
+    window = Window()
+    window.show()
+    sys.exit(app.exec())
