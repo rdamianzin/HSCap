0d0425c7405feba241930982beb139ec17d8a36a
Roberto Damian <122555791+rdamianzin@users.noreply.github.com>
2023-03-21 12:41:20 -0600

    Add files via upload

--- Prueba_carga_modelo.py
+++ Prueba_carga_modelo.py
@@ -20,6 +20,17 @@ import os
            'p11':(60, 0, 110),  'p12':(100, 10, 200),   'p13':(120, 100, 20),   'p14':(100, 160, 10),   'p15':(100, 100, 80),
            'p16':(80, 110, 180),'p17':(40, 100, 200),   'p18':(70, 100, 150),   'p19':(120, 120, 140),  'p20':(100, 0, 0),
            'p21':(150, 0, 0),  'p22':(150, 0, 100),    'p23':(250, 150, 50),   'p24':(250, 50, 50),    'p25':(250, 100, 0)}
+
+data_net = {
+             'bands' : 128, 
+             'classes' : {'p1':(160, 130, 10), 'p2':(230, 240, 110),   'p3':(240, 220, 70),    'p4':(240, 150, 70),    'p5':(160, 80, 10),
+           'p6':(70, 40, 10),   'p7':(30, 30, 120),     'p8':(100, 70, 10),     'p9':(30, 30, 30),      'p10':(30, 0, 60),
+           'p11':(60, 0, 110),  'p12':(100, 10, 200),   'p13':(120, 100, 20),   'p14':(100, 160, 10),   'p15':(100, 100, 80),
+           'p16':(80, 110, 180),'p17':(40, 100, 200),   'p18':(70, 100, 150),   'p19':(120, 120, 140),  'p20':(100, 0, 0),
+           'p21':(150, 0, 0),  'p22':(150, 0, 100),    'p23':(250, 150, 50),   'p24':(250, 50, 50),    'p25':(250, 100, 0)}
+
+with open("./model/t_1_pigment_net.json", "w") as write_file:
+    json.dump(data, write_file)
 '''
 #%% Cargamos la Informacion de modelo para poder cargar el modelo
 
@@ -30,6 +41,17 @@ bands = data['bands']
 
 clases_rgb = data['classes']
 
+classes = []
 
+for x in clases_rgb :
+    classes.append(x)
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
+
+model = Network(bands, classes)
+
+model.load_state_dict(torch.load('./model/t_1_pigment_net.pth'))
+
+model.eval()
 
